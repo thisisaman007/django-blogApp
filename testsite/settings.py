f35053f9a1c7e15a0c5047d3ff8c1819faafdde6
thisisaman007 <thisisaman007@gmail.com>
2020-06-26 18:20:43 +0530

    UPDATED SETTINGS

--- testsite/settings.py
+++ testsite/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y)j1i=v!e6je1mhsm*j%%sq6(ga7#x0^o+9aw1381ygi0*z(9!'
-
+# SECRET_KEY = 'y)j1i=v!e6je1mhsm*j%%sq6(ga7#x0^o+9aw1381ygi0*z(9!'
+SECRET_KEY=os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['blog-app-django1.herokuapp.com']
 
 
 # Application definition
